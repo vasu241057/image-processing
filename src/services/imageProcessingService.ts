28f566faddcdb579f56a01ed0145ca75f125b1da
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-09-07 18:59:58 +0530

    more error handling

--- src/services/imageProcessingService.ts
+++ src/services/imageProcessingService.ts
@@ -12,29 +12,41 @@ async function getS3Client() {
 }
 
 export async function compressImage(url: string): Promise<string> {
-  const response = await axios.get(url, { responseType: "arraybuffer" });
-  const imageBuffer = Buffer.from(response.data);
-  console.log("imagebuffer done");
-
   try {
-    const compressedImageBuffer = await sharp(imageBuffer)
-      .jpeg({ quality: 50 })
-      .toBuffer();
-    const outputFileName = `${uuidv4()}.jpg`;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const imageBuffer = Buffer.from(response.data);
+    console.log("Image buffer obtained successfully");
 
-    const s3 = await getS3Client();
+    try {
+      const compressedImageBuffer = await sharp(imageBuffer)
+        .jpeg({ quality: 50 })
+        .toBuffer();
+      const outputFileName = `${uuidv4()}.jpg`;
 
-    const params = {
-      Bucket: "image-processing-project",
-      Key: outputFileName,
-      Body: compressedImageBuffer,
-      ContentType: "image/jpeg",
-    };
+      const s3 = await getS3Client();
 
-    const uploadResult = await s3.upload(params).promise();
-    return uploadResult.Location;
-  } catch (error) {
-    console.error("Error compressing image:", error);
+      const params = {
+        Bucket: "image-processing-project",
+        Key: outputFileName,
+        Body: compressedImageBuffer,
+        ContentType: "image/jpeg",
+      };
+
+      const uploadResult = await s3.upload(params).promise();
+      return uploadResult.Location;
+    } catch (error: any) {
+      if (error.message.includes("Input buffer has corrupt header")) {
+        console.error("Corrupt image header detected:", error.message);
+        throw new Error(
+          "Corrupt image header detected. Unable to process this image."
+        );
+      } else {
+        console.error("Error compressing image:", error);
+        throw error;
+      }
+    }
+  } catch (error: any) {
+    console.error("Error fetching image from URL:", error);
     throw error;
   }
 }
