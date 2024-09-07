4ef1cfe49adecf003e291f392b7dc4992f7558e5
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-09-07 19:25:23 +0530

    currupt image solved

--- src/controller/uploadController.ts
+++ src/controller/uploadController.ts
@@ -20,52 +20,56 @@ export async function uploadCSV(req: Request, res: Response) {
   }
 
   console.log("CSV validated");
-  console.log(validatedData);
 
   const requestId = uuidv4();
 
   try {
-    // Compress images first
-    const processedData = await Promise.all(
-      validatedData.map(async (record) => {
-        const inputUrls = record.inputImageUrls.split(",");
-        const outputUrls = await Promise.all(
-          inputUrls.map((url: string) => compressImage(url))
-        );
-        return {
-          ...record,
-          outputImageUrls: outputUrls.join(","),
-        };
-      })
-    );
+    for (const record of validatedData) {
+      const inputUrls = record.inputImageUrls.split(",");
+      const outputUrls = [];
 
-    // After successful compression, insert data into the database
-    const processingRequest = await prisma.processingRequest.create({
+      for (const url of inputUrls) {
+        try {
+          const compressedUrl = await compressImage(url);
+          outputUrls.push(compressedUrl);
+        } catch (error: any) {
+          if (error.message.includes("Corrupt image header detected")) {
+            console.error(`Image with URL ${url} is corrupted.`);
+            return res.status(400).json({
+              error: `Image processing stopped. The image at URL ${url} is corrupted.`,
+            });
+          } else {
+            throw error;
+          }
+        }
+      }
+
+      await prisma.product.create({
+        data: {
+          serialNumber: +record.serialNumber,
+          productName: record.productName,
+          inputImageUrls: record.inputImageUrls,
+          outputImageUrls: outputUrls.join(","),
+          processingRequestId: requestId,
+        },
+      });
+    }
+
+    await prisma.processingRequest.create({
       data: {
         id: requestId,
         csvFilePath: file.originalname,
-        products: {
-          create: processedData.map((record) => ({
-            serialNumber: +record.serialNumber,
-            productName: record.productName,
-            inputImageUrls: record.inputImageUrls,
-            outputImageUrls: record.outputImageUrls,
-          })),
-        },
+        status: "COMPLETED",
       },
-      include: {
-        products: true,
-      },
-    });
-
-    await prisma.processingRequest.update({
-      where: { id: requestId },
-      data: { status: "COMPLETED" },
     });
 
     res.json({ requestId });
   } catch (error) {
     console.error("Error processing images or inserting into database:", error);
+    await prisma.processingRequest.update({
+      where: { id: requestId },
+      data: { status: "FAILED" },
+    });
     res
       .status(500)
       .json({ error: "Error processing images or inserting into database." });
